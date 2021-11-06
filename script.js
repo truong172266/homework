7f91fee4622310476e4a6053241c36e22ca7e556
Fang Lee <phanhung18082003@gmail.com>
2021-11-06 16:57:02 +0700

    Finished basic responsive feature

---
+++ script.js
@@ -0,0 +1,10 @@
+function registration() {
+  /*  let a = prompt("Nhập gmail", "@gmail.com");
+  let b = parseInt(prompt("Nhập SĐT:", "0000000000"));
+
+  if (a != "@gmail.com" && b != "0000000000") 
+  alert("Tắt adblock đi bạn, công sức chúng tối không phải để bạn xem chùa :(");
+  document.write(c);
+  console.log(c); */
+  alert("Cảm ơn bạn đã đăng ký nhận tin từ IIN, chúc bạn một ngày tốt lành!");
+}
